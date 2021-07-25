172eb4d5b8a3975d7f1d919ad5f1c437eeaa6eb0
Yaokoo <58417912+Yaokoo@users.noreply.github.com>
2021-07-25 21:43:03 +0800

    Add files via upload

---
+++ ReflectTest03.java
@@ -0,0 +1,26 @@
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+public class ReflectTest03 {
+    /**
+     * 反编译
+     */
+    public static void main(String[] args) throws Exception {
+        StringBuilder s=new StringBuilder();
+
+        Class studentClass=Class.forName("java.lang.Integer");
+        s.append(Modifier.toString(studentClass.getModifiers()) +" class "+studentClass.getSimpleName()+"{\n");
+        Field[] fields=studentClass.getDeclaredFields();
+        for(Field field:fields){
+            s.append("\t");
+            s.append(Modifier.toString(field.getModifiers()));
+            s.append(" ");
+            s.append(field.getType().getSimpleName());
+            s.append(" ");
+            s.append(field.getName());
+            s.append(";"+"\n");
+        }
+        s.append("}");
+        System.out.println(s);
+    }
+}
